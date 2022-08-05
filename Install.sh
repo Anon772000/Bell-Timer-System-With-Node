50484d3be2eeddadd4d4d38eee1678bdcad64eb1
Harrison Breen <hdbreen@gmail.com>
2022-08-06 00:27:43 +1000

    base

---
+++ Install.sh
@@ -0,0 +1,22 @@
+sudo apt-get install pip -y
+sudo apt-get install ffmpeg -y
+sudo pip install pytz
+sudo apt-get install apache2 -y
+sudo apt-get install php7.4 libapache2-mod-php7.4 php7.4-mbstring php7.4-mysql php7.4-curl php7.4-gd php7.4-zip -y
+sudo service apache2 restartsufo
+sudo apt-get install -y systemd
+echo "---finished Downloads Moving Files"
+echo "---Removing Old Files"
+sudo rm -rf /etc/systemd/system/Bell-Timer-System.service
+sudo rm -rf /etc/systemd/system/frpc.service
+sudo rm -rf /etc/frp/frpc.ini
+sudo rm -rf /usr/bin/frpc
+echo "---Moving New Files"
+sudo mv core.service /etc/systemd/system
+echo "---Starting Systemctl"
+sudo systemctl daemon-reload
+sudo systemctl enable Bell-Timer-System.service
+sudo systemctl start Bell-Timer-System.service
+echo "---Moving Web files"
+sudo rm -rf /var/www/html
+sudo mv /etc/Bell-Timer-System/html /var/www/
---
+++ Main.py
@@ -0,0 +1,77 @@
+import json
+import datetime as dt
+from os import system
+import random
+import time
+import threading
+import subprocess
+import logging
+from tokenize import Special
+import pytz
+logging.basicConfig(filename='BellTimerSystem.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.warning('| System started Logging Online')
+# Global Varibales
+webRoot = ""
+globalSettings = json.load(open(webRoot + "html/assets/json/global.json"))
+TIMEZONE = globalSettings['TimeZone']['zone']
+everyday = ("monday", "tuesday", "wednesday","thursday", "friday", "saturday", "sunday")
+workDays = ("monday", "tuesday", "wednesday","thursday", "friday")
+weekend = ( "saturday", "sunday")
+
+
+def TimeLoop():
+    while True:
+            #Grabs real Time
+            todaysdate = dt.datetime.now(pytz.timezone(TIMEZONE))
+            #Creates current hour and min
+            currentTIme = [todaysdate.strftime("%H"), todaysdate.strftime("%M")]
+            globalSettings = json.load(open("/var/www/html/assets/json/global.json"))
+            if globalSettings['EVAC'][EVAC] == True:
+                TimeLoop()
+            specialDay = json.load(open("/var/www/html/assets/json/specialDay.json"))
+            for x in specialDay:
+                if (todaysdate) == x["date"]:
+                    print("bell")
+
+            for x in drillsDates:
+                if (todaysdate) == x["date"]:
+                    print("bell")
+            for x in excludeDates:
+                if (todaysdate) == x["date"]:
+                    print("bell")        
+                        if int(todaysdate.strftime("%M")) == 0:
+                            print('sleeping for 60 seconds')
+                            time.sleep(60)
+                            break
+                        else:
+                            messageArray =[]
+                            for x in sounds:
+                                z = 1
+                                messageArray.insert(int(z),messages[x]['name'])
+                                z+=1
+                            choice = random.choice(messageArray)
+                            logging.warning('| Annoucement Rang '+ choice +', spacing is '+ str(frequency))
+                            print(choice)
+                            running  = threading.Thread(target=play, args=(str(choice),))
+                            logging.warning("Starting Play")
+                            running.start()
+                            time.sleep(60)
+                    
+ 
+                system('clear')
+def play(id):
+    data = json.load(open('/var/www/html/assets/json/messages.json'))
+    for r in data:
+        if id == id:
+            subprocess.call(['ffplay -autoexit -nodisp  /var/www/html/assets/messages/'+id], shell=True)
+            logging.warning('| Message played,-' + data[r]['name'])
+            return
+        else:
+            logging.warning('| ERROR: message did not play')
+            return
+
+if __name__ == "__main__":
+    logging.warning("Creating Threads")
+    x = threading.Thread(target=TimeLoop)
+    logging.warning("Starting Threads")
+    x.start()
