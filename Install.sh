d4ec82e55ca3ec0ca726dac87dd06932edcf2409
Harrison Breen <hdbreen@gmail.com>
2022-09-08 13:31:59 +1000

    fix

--- Install.sh
+++ Install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install pip -y
 sudo apt-get install ffmpeg -y
 sudo pip install pytz
-sudo pip intall pygame
+sudo pip install pygame
 sudo apt-get install apache2 -y
 sudo apt-get install php7.4 libapache2-mod-php7.4 php7.4-mbstring php7.4-mysql php7.4-curl php7.4-gd php7.4-zip -y
 sudo service apache2 restart
